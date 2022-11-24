cd017b1b5a613ad0a4fae1015e104c60f51c4051
EC2 Default User <ec2-user@ip-172-31-8-216.ap-northeast-1.compute.internal>
2022-11-24 14:08:46 +0000

    [Add]会員情報関連実装完了。adminのトップページの編集から行うこと

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,26 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customers_path
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, 
+    :first_name_kana, :is_deleted, :telephone_number, :postal_code, :address,
+    :email, :password)
   end
 end
