2984709ab37f046a5a5834aa9d8a83473cf5ff1f
takathius2 <talahashi760@gmail.com>
2022-12-03 01:52:57 +0000

    [Add]ECサイトの商品一覧完成詳細はこれから作成

--- db/migrate/20221117033932_create_items.rb
+++ db/migrate/20221117033932_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.integer :genre_id, null:false
       t.string :item_name, null: false
       t.string :introduction, null: false
-      t.integer :content_volume, null: false
+      t.string :content_volume, null: false
       t.string :unit, null: false
       t.integer :no_tax_item_price, null: false
       t.boolean :is_active, default: true
--- db/schema.rb
+++ db/schema.rb
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 2022_11_26_023354) do
     t.integer "genre_id", null: false
     t.string "item_name", null: false
     t.string "introduction", null: false
-    t.integer "content_volume", null: false
+    t.string "content_volume", null: false
     t.string "unit", null: false
     t.integer "no_tax_item_price", null: false
     t.boolean "is_active", default: true
