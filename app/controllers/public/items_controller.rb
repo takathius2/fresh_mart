5d3318df1c5cea62d841d89048f087e76fcac219
takathius2 <talahashi760@gmail.com>
2022-12-03 06:17:06 +0000

    [Add]ECサイト標品関連完了

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page])
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :item_name, :introduction, :content_volume, :unit, :no_tax_price, :is_active)
   end
 end
