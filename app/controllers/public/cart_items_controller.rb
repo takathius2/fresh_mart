bdf64ead606ef273bbe08f5adba21449f7ed1a0f
EC2 Default User <ec2-user@ip-172-31-8-216.ap-northeast-1.compute.internal>
2022-12-05 08:12:50 +0000

    [Add]配送先と住所の部分テンプレート化実装済み

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,51 @@
 class Public::CartItemsController < ApplicationController
   def index
+      @cart_items = current_customer.cart_items
+      @items = Item.page(params[:page])
+      #合計金額の初期値は0円
+      @total = 0
+  end
+  
+  def create
+    @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      
+    if @cart_item
+      #カートにitemが存在したらamountに新しいCart_itemのamountを足す
+      @cart_item.amount += CartItem.new(cart_item_params).amount
+    else
+      #カートにitemが無ければ新しく作成する
+      @cart_item = CartItem.new(cart_item_params)
+    end
+    #ログインcustomerのみ更新できるようにするため分岐の外に記述する
+    if @cart_item.customer = current_customer
+      p @cart_item
+      @cart_item.save
+      redirect_to cart_items_path
+    else
+      redirect_to new_customer_session_path
+    end
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @item = Item.all
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy!
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- db/migrate/20221117032138_create_addresses.rb
+++ db/migrate/20221117032138_create_addresses.rb
@@ -2,9 +2,9 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
       t.integer :customer_id
-      t.string :postal_code, null: false
-    	t.string :destination, null: false
-    	t.string :name, null: false
+      t.string :postal_code
+    	t.string :destination
+    	t.string :name
 
       t.timestamps
     end
