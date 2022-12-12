1a56c2b498798a5052bac6288ebbdbc47c81c8c2
takathius2 <talahashi760@gmail.com>
2022-12-12 00:09:26 +0000

    [Add]顧客画面終わり。管理者側の注文詳細作成してください

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
     # belongs_to :customer
-    # belongs_to :order
+    #  belongs_to :order
     
     # 宛先氏名・郵便番号・住所の記入漏れチェック
     validates :postal_name, presence: true, length: { minimum: 1 }
