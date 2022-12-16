3efd633bc5cf84fb88581f1b3a9152c90322230a
takathius2 <talahashi760@gmail.com>
2022-12-16 14:15:13 +0000

    [Add]エラー文の翻訳完了・誤字修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FreshMart
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Asia/Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
